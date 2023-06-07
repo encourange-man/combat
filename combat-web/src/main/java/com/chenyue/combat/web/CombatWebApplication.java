cc7d67eb361b674cc56bbd9160692dd62a204c8e
machen <machen.ma@alibaba-inc.com>
2023-06-07 19:16:25 +0800

    初始化项目

---
+++ combat-web/src/main/java/com/chenyue/combat/web/CombatWebApplication.java
@@ -0,0 +1,13 @@
+package com.chenyue.combat.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CombatWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CombatWebApplication.class, args);
+    }
+
+}
---
+++ combat-web/src/test/java/com/chenyue/combat/web/CombatWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chenyue.combat.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CombatWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
